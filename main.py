7f418320a6e0206a9c714e6f8e3dbff18f58e4f6
van799 <lexxD@list.ru>
2023-03-03 00:53:25 +0300

    sleep

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from implementation.tasks.create_dir_task import CreateDirTask
 from implementation.tasks.delete_file_task import DeleteFileTask
 from implementation.tasks.get_weather_task import GetWeatherTask
 
+from implementation.tasks.empty_task import EmptyTask
+from datetime import datetime, timedelta
+
 from implementation.queue_processor import QueueProcessor
 from implementation.file_read_write import FileReadWrite
 from implementation.job_json_repository import JobJsonRepository
@@ -28,6 +31,8 @@ logger = Logger('data/logger.log', 'w', logging.DEBUG)
 read_writer = FileReadWrite(config.filename)
 #     """Класс нужен для создания класса такса при считывании не выполненных TASK из файла."""
 task_factory = TaskFactory()
+task_factory.register_task(name_task='empty task',
+                           create_task=lambda p: EmptyTask(p))
 task_factory.register_task(name_task='get weather',
                            create_task=lambda p: GetWeatherTask(p))
 task_factory.register_task(name_task='delete file',
@@ -41,23 +46,28 @@ scheduler = Scheduler(logger, QueueProcessor(logger, JobQueueDispatcher([])), re
 
 if not os.path.exists(config.filename) or saved_job_count <= 0:
     #     Запуск TASK в dependencies
-    job_create_dir = Job(CreateDirTask('data/create_dir'), tries=5)
-
-    job_create_file = Job(CreateFileTask('data/create_dir/create_file.txt'),
-                          dependencies=[job_create_dir.job_id], tries=5)
-    job_delete_file = Job(DeleteFileTask('data/create_file/create_file.txt'), dependencies=[job_create_file.job_id],
-                          tries=5)
-
-    scheduler.schedule(job_create_dir)
-    scheduler.schedule(job_create_file)
-    scheduler.schedule(job_delete_file)
-    #     Запуск TASK
-    scheduler.schedule(Job(task=CreateDirTask('data/create_dir'), max_working_time=-1, tries=0))
-    scheduler.schedule(Job(task=CreateFileTask('data/create_dir/create_file_weather.txt'), max_working_time=-1,
-                           tries=0))
-    scheduler.schedule(Job(task=GetWeatherTask('data/create_dir/create_file_weather.txt'), max_working_time=-1,
-                           tries=0))
+    # job_create_dir = Job(CreateDirTask('data/create_dir'), tries=5)
+    #
+    # job_create_file = Job(CreateFileTask('data/create_dir/create_file.txt'),
+    #                       dependencies=[job_create_dir.job_id], tries=5)
+    # job_delete_file = Job(DeleteFileTask('data/create_file/create_file.txt'), dependencies=[job_create_file.job_id],
+    #                       tries=5)
+    #
+    # scheduler.schedule(job_create_dir)
+    # scheduler.schedule(job_create_file)
+    # scheduler.schedule(job_delete_file)
+    # #     Запуск TASK
+    # scheduler.schedule(Job(task=CreateDirTask('data/create_dir'), max_working_time=-1, tries=0))
+    # scheduler.schedule(Job(task=CreateFileTask('data/create_dir/create_file_weather.txt'), max_working_time=-1,
+    #                        tries=0))
+    # scheduler.schedule(Job(task=GetWeatherTask('data/create_dir/create_file_weather.txt'), max_working_time=-1,
+    #                        tries=0))
+    # test time
+    # scheduler.schedule(
+    #     Job(task=EmptyTask(), max_working_time=-1, tries=0, start_at=datetime.now() + timedelta(minutes=1)))
+    scheduler.schedule(
+        Job(task=EmptyTask('param'), max_working_time=-1, tries=0, start_at=datetime.now() + timedelta(minutes=1)))
 
 scheduler.run()
-time.sleep(10)
+time.sleep(120)
 scheduler.stop()
--- tests/test_core_scheduler.py
+++ tests/test_core_scheduler.py
@@ -3,6 +3,7 @@ import unittest
 from scheduler import Scheduler
 from core.job import Job
 
+from tests.common.test_logger import TestLogger
 from tests.common.test_empty_task import TestEmptyTask
 from tests.common.test_empty_task_always_not_complete import TestEmptyTaskAlwaysNotComplete
 from tests.common.test_job_repository import TestJobRepository
@@ -12,6 +13,7 @@ from implementation.job_queue_dispatcher import JobQueueDispatcher
 
 class SchedulerTest(unittest.TestCase):
     def test_scheduler_load_jobs_when_start(self):
+
         jobs = [
             Job(task=TestEmptyTask(), max_working_time=-1, tries=0),
             Job(task=TestEmptyTask(), max_working_time=-1, tries=0),
@@ -20,14 +22,14 @@ class SchedulerTest(unittest.TestCase):
         ]
         job_repository = TestJobRepository(items=jobs)
         queue = []
-        scheduler = Scheduler(TestQueueProcessor(JobQueueDispatcher(queue)), job_repository)
+        scheduler = Scheduler(TestLogger(), TestQueueProcessor(JobQueueDispatcher(queue)), job_repository)
         scheduler.run()
         self.assertEqual(len(queue), len(jobs))
 
     def test_scheduler_save_jobs_when_stop_and_jobs_is_not_done(self):
         max_job_count = 5
         job_repository = TestJobRepository()
-        scheduler = Scheduler(TestQueueProcessor(JobQueueDispatcher([])), job_repository)
+        scheduler = Scheduler(TestLogger(), TestQueueProcessor(JobQueueDispatcher([])), job_repository)
         for i in range(max_job_count):
             scheduler.schedule(Job(task=TestEmptyTaskAlwaysNotComplete(), max_working_time=-1, tries=0))
         scheduler.stop()
